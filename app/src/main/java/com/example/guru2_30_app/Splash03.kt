afbbf2d7020cc0aa46566597dbd822d31b374827
leehyominlee <ljh2454fd@gmail.com>
2023-07-28 17:43:07 +0900

    .

--- app/src/main/java/com/example/guru2_30_app/Splash03.kt
+++ app/src/main/java/com/example/guru2_30_app/Splash03.kt
@@ -10,7 +10,7 @@ class Splash03 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash03)
 
-        findViewById<View>(R.id.StartButton).setOnClickListener {
+        findViewById<View>(R.id.btn03).setOnClickListener {
             // 다른 화면으로 전환하기 위한 Intent 생성
             val intent = Intent(this@Splash03, Splash04::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/guru2_30_app/Splash04.kt
+++ app/src/main/java/com/example/guru2_30_app/Splash04.kt
@@ -1,11 +1,24 @@
 package com.example.guru2_30_app
 
+import android.annotation.SuppressLint
+
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent;
+import android.view.View;
+
 
 class Splash04 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash04)
+
+        findViewById<View>(R.id.btn04).setOnClickListener {
+            // 다른 화면으로 전환하기 위한 Intent 생성
+            val intent = Intent(this@Splash04, Splash05::class.java)
+            startActivity(intent)
+        }
     }
 }
