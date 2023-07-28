5977484d7ebf1027b9c2d023cda265aaa7d761c9
babogyeong <jbk021010@gmail.com>
2023-07-28 14:58:09 +0900

    spalsh1,3 check

---
+++ app/src/main/java/com/example/guru2_30_app/Splash03.kt
@@ -0,0 +1,11 @@
+package com.example.guru2_30_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash03 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash03)
+    }
+}
