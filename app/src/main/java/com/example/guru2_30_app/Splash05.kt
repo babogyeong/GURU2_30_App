2b86e3578b2d6bc9a434e7fbc130eb6efc0f9f82
leehyominlee <ljh2454fd@gmail.com>
2023-07-28 17:56:08 +0900

    splash01~05까지 앞으로 수정할 버튼id까지 미리 반영해서 적음

--- app/src/main/java/com/example/guru2_30_app/Splash05.kt
+++ app/src/main/java/com/example/guru2_30_app/Splash05.kt
@@ -1,11 +1,19 @@
 package com.example.guru2_30_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Splash05 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash05)
+
+        findViewById<View>(R.id.StartButton).setOnClickListener {
+            // 다른 화면으로 전환하기 위한 Intent 생성
+            val intent = Intent(this@Splash05, main::class.java)
+            startActivity(intent)
+        }
     }
 }
