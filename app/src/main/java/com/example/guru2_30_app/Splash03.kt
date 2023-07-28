85f8816bc00d236503972ac814d2621cb7b453cb
Lee Chaeyoung <0929alice@naver.com>
2023-07-28 21:52:12 +0900

    btn id수정했으나 이번엔 splash3,4,5가 동기화됨

--- app/src/main/java/com/example/guru2_30_app/Splash03.kt
+++ app/src/main/java/com/example/guru2_30_app/Splash03.kt
@@ -10,7 +10,7 @@ class Splash03 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash03)
 
-        findViewById<View>(R.id.btn03).setOnClickListener {
+        findViewById<View>(R.id.btn05).setOnClickListener {
             // 다른 화면으로 전환하기 위한 Intent 생성
             val intent = Intent(this@Splash03, Splash04::class.java)
             startActivity(intent)
--- app/src/main/java/com/example/guru2_30_app/Splash04.kt
+++ app/src/main/java/com/example/guru2_30_app/Splash04.kt
@@ -12,7 +12,7 @@ class Splash04 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash04)
 
-        findViewById<View>(R.id.btn04).setOnClickListener {
+        findViewById<View>(R.id.btn02).setOnClickListener {
             // 다른 화면으로 전환하기 위한 Intent 생성
             val intent = Intent(this@Splash04, Splash05::class.java)
             startActivity(intent)
