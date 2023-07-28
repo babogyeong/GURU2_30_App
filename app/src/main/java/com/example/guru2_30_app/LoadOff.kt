d16717e2bf5b1916aa3a0bc3625ced79900dcb56
leehyominlee <ljh2454fd@gmail.com>
2023-07-28 22:27:38 +0900

    LoadOff->LoadOn->Splash01 연결 코드 구현

--- app/src/main/java/com/example/guru2_30_app/LoadOff.kt
+++ app/src/main/java/com/example/guru2_30_app/LoadOff.kt
@@ -1,10 +1,22 @@
 package com.example.guru2_30_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LoadOff : AppCompatActivity() {
+
+    private val delayMillis: Long = 2000 // 2초
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loadoff)
+        setContentView(R.layout.activity_load_off)
+
+        //2초 후에 자동으로 LoadOn으로 전환
+        Handler().postDelayed({
+            val intent = Intent(this@LoadOff, LoadOn::class.java)
+            startActivity(intent)
+            finish()
+        }, delayMillis)
     }
+}
--- app/src/main/java/com/example/guru2_30_app/LoadOn.kt
+++ app/src/main/java/com/example/guru2_30_app/LoadOn.kt
@@ -1,10 +1,23 @@
 package com.example.guru2_30_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class LoadOn : AppCompatActivity() {
+
+    private val delayMillis: Long = 3000 //3초
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_loadon)
+
+        //3초후에 자동으로 Splash01로 전환
+        Handler().postDelayed({
+            val intent = Intent(this@LoadOn, Splash01::class.java)
+            startActivity(intent)
+            finish()
+        }, delayMillis)
     }
+
