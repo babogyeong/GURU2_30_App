8eb4c5797f98038a7f1a4236d23dd586cc80dae3
babogyeong <jbk021010@gmail.com>
2023-07-29 18:20:13 +0900

    main 화면 사이드바 코딩 함( 사이드바의 추가적인 기능은 아직 안 넣음) drawable폴더 대문자>소문자로 변환해야함

--- app/src/main/java/com/example/guru2_30_app/MainActivity.kt
+++ app/src/main/java/com/example/guru2_30_app/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.guru2_30_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sidebarLayout: LinearLayout
+    private lateinit var btnSidebar: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sidebarLayout = findViewById(R.id.sidebarLayout)
+        btnSidebar = findViewById(R.id.btn_sidebar)
+
+        btnSidebar.setOnClickListener {
+            if (sidebarLayout.visibility == View.VISIBLE) {
+                sidebarLayout.visibility = View.GONE
+            } else {
+                sidebarLayout.visibility = View.VISIBLE
+            }
+        }
     }
 }
